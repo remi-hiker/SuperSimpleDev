2ad2e3a2ed08bb04dfb04e69c030b7db8b87243e
remi-hiker <Remi.EJ@outlook.com>
2025-03-07 00:21:57 +0000

    10h-onwards finished

--- chapter_10/10-exercises-h-onwards/10h-onwards.js
+++ chapter_10/10-exercises-h-onwards/10h-onwards.js
@@ -4,27 +4,26 @@ function handleCostKeyDown(event) {
     }
 }
 
-
-function subscribe() {
-    const buttonElement = document.querySelector('.js-subscribe-button');
-    if(buttonElement.innerHTML === 'Subscribe'){
-        buttonElement.innerHTML = 'Subscribed';
-        buttonElement.classList.add('is-subscribed');
-    }         
-    else {
-        buttonElement.innerHTML = 'Subscribe';
-        buttonElement.classList.remove('is-subscribed');
-    }
-}
-
 function calculateTotal() {
     const inputElement = document.querySelector('.js-cost-input');
     let cost = Number(inputElement.value);
 
-    if(cost < 40) {
-        cost = cost + 10;
+    if(cost < 0) {
+        document.querySelector('.js-total-cost')
+        .innerHTML = ``;
+        
+        document.querySelector('.js-error-message')
+            .innerHTML = 'Error: cost cannot be lower than $0';
     }
 
-    document.querySelector('.js-total-cost')
-        .innerHTML = `$${cost}`;
+    if(0 < cost && cost < 40) {
+        cost = cost + 10;
+        document.querySelector('.js-total-cost')
+            .innerHTML = `$${cost}`;
+
+        document.querySelector('.js-error-message')
+            .innerHTML = '';
+    }
+
+    
 }
